4aae11bc4e7f2952bba9d2bf9ce434602cc11056
nico <hans.dieter.schmidt.wurst@gmail.com>
2024-08-06 12:40:43 +0200

    Added Library for PKRU

---
+++ l4_packages/mpklibrary/lib/src/pkrulib.cc
@@ -0,0 +1,40 @@
+#include "pkrulib.h"
+
+extern "C" unsigned int pkru_lib_read_PKRU(void);
+extern "C" void pkru_lib_write_PKRU(unsigned int pkruValue);
+
+void PKRUlib::write(unsigned int new_value)
+{
+    pkru_lib_write_PKRU(new_value);
+}
+
+unsigned int PKRUlib::read(void)
+{
+    return pkru_lib_read_PKRU();
+}
+
+void PKRUlib::write_partial(unsigned int new_value, unsigned int mask)
+{
+    unsigned int current_pkru = PKRUlib::read();
+    unsigned int zeroed_mask_current_pkru = current_pkru & ~mask; // zero to change bits with or
+    unsigned int masked_new_value = new_value & mask;             // only change bits represented by the mask
+    PKRUlib::write(zeroed_mask_current_pkru | masked_new_value);
+}
+
+void PKRUlib::write(Key key, Rights rights)
+{
+    write_partial(rights, key);
+}
+
+bool PKRUlib::has_rights(Key key, Rights rights)
+{
+    unsigned int current_pkru = PKRUlib::read();
+    unsigned int only_key_rights = current_pkru & key;
+    unsigned int desired_key_rights = key & rights;
+
+    // there is no bit operator to check for allrights (well not or but why use that) or Access(and)WriteDisable
+    if (rights == PKRUlib::Rights::AllRights || rights == PKRUlib::Rights::AccessWriteDisable)
+        return only_key_rights == desired_key_rights;
+    else
+        return only_key_rights & desired_key_rights;
+}
---
+++ l4_packages/mpklibrary/lib/src/pkrulib.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class PKRUlib {
+public:
+    enum Key{
+        Key_0  = 0b11,
+        Key_1  = 0b1100,
+        Key_2  = 0b110000,
+        Key_3  = 0b11000000,
+        Key_4  = 0b1100000000,
+        Key_5  = 0b110000000000,
+        Key_6  = 0b11000000000000,
+        Key_7  = 0b1100000000000000,
+        Key_8  = 0b110000000000000000,
+        Key_9  = 0b11000000000000000000,
+        Key_10 = 0b1100000000000000000000,
+        Key_11 = 0b110000000000000000000000,
+        Key_12 = 0b11000000000000000000000000,
+        Key_13 = 0b1100000000000000000000000000,
+        Key_14 = 0b110000000000000000000000000000,
+        Key_15 = 0b11000000000000000000000000000000,
+    };
+    enum Rights{
+        AllRights = 0,
+        AccessDisable = 0b01010101010101010101010101010101,
+        WriteDisable =  0b10101010101010101010101010101010,
+        AccessWriteDisable = AccessDisable | WriteDisable,
+    };
+
+    static void write(unsigned int new_value);
+    static void write_partial(unsigned int new_value, unsigned int key_mask);
+    static unsigned int read(void);
+
+    static void write(Key key, Rights rights);
+    static bool has_rights(Key key, Rights rights);
+};
